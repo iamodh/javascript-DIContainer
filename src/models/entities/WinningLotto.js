ea41102874d514ed957e7365581fdaf3112243da
Donghwan Oh <imodh97@gmail.com>
2025-11-11 15:07:35 +0900

    feat(winningLotto): 로또 클래스와의 협력을 위한 기능 추가
    Lotto와 WinningLotto는 등 수 계산, 공통 번호 계산의 다른 책임을 가지고 있음
    따라서 상속 관계 제거

--- src/models/entities/WinningLotto.js
+++ src/models/entities/WinningLotto.js
@@ -1,14 +1,30 @@
-import Lotto from '../entities/Lotto.js';
 import ERROR_MESSAGES from '../../constants/errorMessages.js';
+import LOTTO_CONFIG from '../../constants/lottoConfig.js';
 
-class WinningLotto extends Lotto {
+class WinningLotto {
+  #winningNumbers;
   #bonusNumber;
 
   constructor(numbers, bonusNumber) {
-    super(numbers);
-
+    this.#validateNumbersCount(numbers);
+    this.#validateNumbersDuplicates(numbers);
     this.#validateBonusNumberDuplicates(numbers, bonusNumber);
+
+    this.#winningNumbers = numbers;
     this.#bonusNumber = bonusNumber;
+  }
+
+  #validateNumbersCount(numbers) {
+    if (numbers.length !== LOTTO_CONFIG.NUMBERS_COUNT) {
+      throw new Error(ERROR_MESSAGES.NUMBERS_INVALID_COUNT);
+    }
+  }
+
+  #validateNumbersDuplicates(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error(ERROR_MESSAGES.NUMBERS_DUPLICATES);
+    }
   }
 
   #validateBonusNumberDuplicates(numbers, bonusNumber) {
@@ -17,8 +33,17 @@ class WinningLotto extends Lotto {
     }
   }
 
-  getBonusNumber() {
-    return this.#bonusNumber;
+  calculateMatchCount(lottoNumbers) {
+    const totalCounts = this.#winningNumbers.length + lottoNumbers.length;
+
+    const unionCounts = new Set([...this.#winningNumbers, ...lottoNumbers])
+      .size;
+
+    return totalCounts - unionCounts;
+  }
+
+  calculateBonusMatch(lottoNumbers) {
+    return lottoNumbers.includes(this.#bonusNumber);
   }
 }
 
