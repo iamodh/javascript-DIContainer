3add57da971ebb1437baf096a285a0030a774d02
Donghwan Oh <imodh97@gmail.com>
2025-11-12 17:02:32 +0900

    feat(app): fixedStrategy 활용을 위해 개발 버전 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
+import DEV_CONFIG from './constants/devConfig.js';
 import LOTTO_CONFIG from './constants/lottoConfig.js';
 import LottoController from './controllers/LottoController.js';
 import DIContainer from './DIContainer.js';
 import LottoResult from './models/domains/LottoResult.js';
-// import FixedStrategy from './models/services/FixedStrategy.js';
+import FixedStrategy from './models/services/FixedStrategy.js';
 import LottoMachine from './models/services/LottoMachine.js';
 import RandomStrategy from './models/services/RandomStrategy.js';
 import InputView from './views/InputView.js';
 import OutputView from './views/OutputView.js';
+
 class App {
-  async run() {
+  async run(env) {
     const container = new DIContainer();
     container.register('inputView', InputView);
     container.register('outputView', OutputView);
-    // container.register('fixedStrategy', FixedStrategy, [[1, 2, 3, 4, 5, 6]]);
-    container.register('randomStrategy', RandomStrategy, [
-      LOTTO_CONFIG.NUMBER_RANGE_FROM,
-      LOTTO_CONFIG.NUMBER_RANGE_TO,
-      LOTTO_CONFIG.NUMBERS_COUNT,
-    ]);
-    container.register('lottoMachine', LottoMachine, ['randomStrategy']);
-    container.register('lottoResult', LottoResult);
 
+    if (env === 'dev') {
+      container.register('fixedStrategy', FixedStrategy, [
+        DEV_CONFIG.FIXED_NUMBERS,
+      ]);
+      container.register('lottoMachine', LottoMachine, ['fixedStrategy']);
+    } else {
+      container.register('randomStrategy', RandomStrategy, [
+        LOTTO_CONFIG.NUMBER_RANGE_FROM,
+        LOTTO_CONFIG.NUMBER_RANGE_TO,
+        LOTTO_CONFIG.NUMBERS_COUNT,
+      ]);
+      container.register('lottoMachine', LottoMachine, ['randomStrategy']);
+    }
+
+    container.register('lottoResult', LottoResult);
     container.register('lottoController', LottoController, [
       'inputView',
       'outputView',
---
+++ src/constants/devConfig.js
@@ -0,0 +1,5 @@
+const DEV_CONFIG = {
+  FIXED_NUMBERS: [1, 2, 3, 4, 5, 6],
+};
+
+export default DEV_CONFIG;
